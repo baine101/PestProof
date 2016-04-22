0a4f7f8c3814040f069d52b49b689f13175e736c
Roachykins <roach_trivium@hotmail.co.uk>
2016-04-22 16:11:31 +0100

    Pushed new home page HTML

--- index.php
+++ index.php
@@ -40,159 +40,168 @@ require 'nav.php';
 <!-- heading -->
 
 <!--services section start -->
-    <div class="row section background-1 Services">
-        <div class="col-lg-12 centered">
-            <h2 class="text-center hr-heading">Services</h2>
+    <div class="container-fluid section">
+        <div class="text-center well well-custom">
+		
+            <h2><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Services</h2>
+		
+			<!-- Start of Services Panel -->
+			<div class='panel panel-custom  services-wrapper'>
+			<!-- 1st Services Row Start -->
+			<div class='row'>
+			<div class="panel-body">
+			<p>We offer A range of commercial pest management solutions and environmental services, Tailored to meet the specific requirements of our customers and their sectors .</p>
+			
+			  <div class='col-sm-1'></div>
+			  
+			  <a href="servicesd41d.html?#pestMan" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/poison.jpg);" class="col-md-3 jumbotron-services">
+						<p class="content-box">Pest Management</p>
+					</div>
+				</a>
+			  
+			  <a href="servicesd41d.html?#inspection" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/inspection.jpg);" class="col-md-3 jumbotron-services">
+						<p class="content-box">Inspection Service</p>
+					</div>
+				</a>
 
-            <p class="text-center">We offer A range of commercial pest management solutions and environmental services, Tailored to meet the specific requirements of our customers and their sectors .</p>
+              <a href="servicesd41d.html?#birdCon" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/birdnet.jpg);" class="col-md-3 jumbotron-services">
+						<p class="content-box">Bird Control & Netting</p>
+					</div>
+			  </a>
+			  
+              <a href="servicesd41d.html?#rodentCon" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/Rat.jpg);" class="col-md-3 jumbotron-services">
+						<p class="content-box">Rodent Control</p>
+					</div>
+				</a>
+
+              <a href="servicesd41d.html?#insectCon" class="a-section">
+                  <div style="background-image: url(images/jumboxes/services/insect.jpg);" class="col-md-3 jumbotron-services">
+						<p class="content-box">Insect control</p>
+				  </div>
+              </a>	
+			  
+			</div>
+			
+			</div>
+				
+			<!-- 2nd Services Row Start -->
+			<div class='row'>
+				 <div class='col-sm-1'></div>
+                <a href="servicesd41d.html?#plasticStrip" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/plasticStrip.jpg);" class="col-md-2  jumbotron-services">
+						<p class="content-box">Plastic Strip Curtains & Chain Linked Doors</p>
+					</div>
+				</a>
 
 
-            <div class="row center-block">
+                <a href="servicesd41d.html?#flyScreen" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/flyscreen.jpg);" class="col-md-2  jumbotron-services">
+						<p class="content-box">Fly Screen & Electric Fly Control Units</p>
+					</div>
+				</a>
 
-                <!-- ********* services blocks -->
-                <div class="col-sm-1"></div>
+                <a href="servicesd41d.html?#fumigation" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/biohazard.png);" class="col-md-2  jumbotron-services">
+						<p class="content-box">Fumigation</p>
+					</div>
+				</a>
 
-                <a href="services.php?#pestMan" class="a-section"><div style="background-image: url(../images/jumboxes/services/poison.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Pest Management</p>
-                </div></a>
+                <a href="servicesd41d.html?#pestAware" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/training.jpg);" class="col-md-2  jumbotron-services">
+						<p class="content-box">Pest Awareness Training</p>
+					</div>
+				</a>
 
-                <a href="services.php?#inspection" class="a-section"><div style="background-image: url(../images/jumboxes/services/inspection.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Inspection Service</p>
-                </div></a>
-
-                <a href="services.php?#birdCon" class="a-section"><div style="background-image: url(../images/jumboxes/services/birdnet.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Bird Control & Netting</p>
-                </div></a>
-
-                <a href="services.php?#rodentCon" class="a-section"><div style="background-image: url(../images/jumboxes/services/Rat.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Rodent Control</p>
-                </div></a>
-
-                <a href="services.php?#insectCon" style="background-image: url(../images/jumboxes/services/insect.jpg);" class="a-section col-lg-2 jumbotron-services">
-                    <p class="content-box">Insect control</p>
-                </a>
+				<a href="servicesd41d.html?#hygine" class="a-section">
+					<div style="background-image: url(images/jumboxes/services/hygine.jpg);" class="col-md-2  jumbotron-services">
+							<p class="content-box">Hygiene Services</p>
+					</div>
+				</a>
                 <!-- close row -->
             </div>
-
-
-            <div class="row center-block">
-
-                <div class="col-sm-1"></div>
-
-                <a href="services.php?#plasticStrip" class="a-section"><div style="background-image: url(../images/jumboxes/services/plasticStrip.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Plastic Strip Curtains & Chain Linked Doors</p>
-                </div></a>
-
-                <a href="services.php?#flyScreen" class="a-section"><div style="background-image: url(../images/jumboxes/services/flyscreen.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Fly Screen & Electric Fly Control Units</p>
-                </div></a>
-
-                <a href="services.php?#fumigation" class="a-section"><div style="background-image: url(../images/jumboxes/services/biohazard.png);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Fumigation</p>
-                </div></a>
-
-                <a href="services.php?#pestAware" class="a-section"><div style="background-image: url(../images/jumboxes/services/training.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Pest Awareness Training</p>
-                </div></a>
-
-            <a href="services.php?#hygine" class="a-section"><div style="background-image: url(../images/jumboxes/services/hygine.jpg);" class="col-lg-2 jumbotron-services">
-                    <p class="content-box">Hygiene Services</p>
-                </div></a>
+			<!-- End services wrapper -->
+			</div>
+		</div>
+ </div>
 
 
 
-
-                <!-- close row -->
-            </div>
-            <br>
-        </div>
-    </div>
-    <!--close services section -->
+<!--close services section -->
 
 
 
 
     <!--About section start -->
-    <div class="row section background-2" id="about">
-        <div class="col-lg-1 center-block">
-        </div>
-        <div class="col-lg-10 centered">
-            <br>
-            <br>
-            <br>
-
-
-
-            <h2 class="text-center hr-heading">About Us</h2>
-
-            <p style="width: 90%" class="text-center">Formed over 18 years ago, Pestproof is a leading supplier of pest control and environmental services located in the North West.
-
+    <div class="row well" id="about">
+		<div class='panel panel-custom about-wrapper'>
+    		<div class="panel-body">
+                <h2 class="text-center fancy-heading">
+                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> <!-- Header Icon -->
+                About Us    <!-- Header Title -->
+                </h2>
+    
+                <p class="col-md-6">
+    			Formed over 18 years ago, Pestproof is a leading supplier of pest control and environmental services located in the North West.
                 We offer the complete pest prevention package, tailoring each integrated pest management specification to meet the specific needs of our customers. We are full members of the BPCA and our flagship Elite Pest Management service is fully compliant with BRC audits for major retailers including Tesco and M&S. We are also accredited to the prestigious ISO 9001:2000 quality systems; proof of our commitment to providing an accountable and quality service for our customers.
-
-                Many of our customers are market leaders in their industries and they expect exceptionally high levels of quality and service. They value our responsiveness, our flexibility, the professionalism and knowledge of our team and the high quality delivery of our proactive service.
-
-                Operating at the forefront of our industry, we are committed to ethical practices and processes utilising the latest technologies and innovations. Our Pest Technicians are highly trained and experienced with ongoing Health and Safety training as standard including the newly introduced Safety Passport.
-
-                Our integrated pest management service is complemented by a range of specialist cleaning and washroom hygiene services. </p>
-
-            <div class="col-lg-12">
-
-
+    			Many of our customers are market leaders in their industries and they expect exceptionally high levels of quality and service.
+               </p>
+    
+                <p class="col-md-6">
+    			 They value our responsiveness, our flexibility, the professionalism and knowledge of our team and the high quality delivery of our proactive service.
+    			Operating at the forefront of our industry, we are committed to ethical practices and processes utilising the latest technologies and innovations. Our Pest Technicians are highly trained and experienced with ongoing Health and Safety training as standard including the newly introduced Safety Passport.
+                Our integrated pest management service is complemented by a range of specialist cleaning and washroom hygiene services. 
+                </p>
             </div>
         </div>
-        <!--close about section -->
+	 <!--close about section -->
     </div>
 
-
-
-
-
-
-
-<!-- cleaning section start-->
-<div class="row section background-1" id="cleaning">
-    <div class="col-lg-1 center-block"></div>
-    <div class="col-lg-10 center-block">
-        <br>
-        <br>
-        <br>
-
-        <h2 class="text-center hr-heading">Cleaning & Removals</h2>
-        <div class="col-lg-12 text-center">
-            <img style="border-radius: 25px;width: 60%;height: 45%" src="../images/Before&after.jpg">
-        </div>
-        <div class="col-lg-12 text-center">
-            <br>
-            <p style="width: 90%" class="text-center">To complement our pest management and hygiene services, we also offer specialist cleaning and office cleaning services. We are equipped to carry out all types of cleaning including high-level cleaning in factories, silo servicing and silo house deep cleaning as well as a specialist team for void clearances. We are qualified to carry out work requiring specialised access equipment, such as abseiling techniques and Bosun’s chair work. To minimise disruption, our cleaning services can be carried out during shutdowns, weekends or through the night.</p>
-            <br>
-
-        </div>
-
-    </div>
+<!-- New cleaning section start-->
+<div class="row well" id="cleaning">
+    <div class='panel panel-custom about-wrapper'>
+		<div class="panel-body">
+		    
+        <h2 class="text-center fancy-heading">
+        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span> <!-- Header Icon -->
+        Cleaning & Removals  <!-- Header Title -->
+        </h2> <
+        
+        <div class="col-lg-12 ">
+			<p>
+				To complement our pest management and hygiene services, 
+				we also offer specialist cleaning and office cleaning services. 
+				We are equipped to carry out all types of cleaning including high-level cleaning in factories, 
+				silo servicing and silo house deep cleaning as well as a specialist team for void clearances. 
+				We are qualified to carry out work requiring specialised access equipment, such as abseiling techniques and Bosun’s chair work.
+				To minimise disruption, our cleaning services can be carried out during shutdowns, weekends or through the night.
+			</p>
+            <center><img style="border-radius: 25px;width: 60%;height: 45%" src="images/Before%26after.jpg"></center>
+		</div>
+	</div>
+	</div>
 </div>
 
+   <!--contact us title-->
 
-    <!--contact us title-->
-
-    <div class="row text-center background-2" id="contact">
-        <h2 class="hr-heading">Contact Us</h2>
-    </div>
-
+<div class="row text-center" id="contact">
+	<div class='panel panel-custom main-wrapper'>
+		<div class="panel-body">
+        <h2 class="fancy-heading">Contact Us</h2>
     <!--close title -->
-    <div class="row background-2">
+    <div class="row">
         <div class="col-lg-6 text-center background-2">
 
             <h3 class="hr-heading">Address & Numbers</h3>
-
-
             <b>Address:</b><address>Mitre Street<br>
                 Failsworth<br>
                 Manchester<br>
                 M35 9BY <br></address>
             <b>Telephone:</b> 0161 684 9451<br>
             <b>Fax :</b>0161 947 0485
-
-
 
         </div>
 
@@ -203,7 +212,7 @@ require 'nav.php';
             <h3 class="hr-heading">Enquiry</h3>
 
 
-            <form action="controllers/enquiry.php" method="post">
+            <form action="http://176.35.152.229/controllers/enquiry.php" method="post">
                 <div class="input-group">
                 <span class="input-group-addon" id="name">Name :</span>
                 <input type="text" class="form-control" name="name" placeholder="Name" aria-describedby="name">
@@ -222,17 +231,17 @@ require 'nav.php';
                 </div>
                 <input class="btn-submit" type="submit" name="submit" value="Submit Enquiry">
             </form>
-            <?php
-
-
-            ?>
-
+            
         </div>
 
         <!--close enquiry form -->
-
     </div>
-
+<!--end enquiry row-->
+</div>
+<!-- End Panel -->
+</div>
+<!-- End contact section -->
+ </div>
 
 <!--scroll to top button-->
 <footer class="navbar-fixed-bottom">
